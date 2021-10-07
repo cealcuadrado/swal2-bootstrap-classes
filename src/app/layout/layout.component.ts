fd2ec8a2076a4fd7080d595ad50342799f487666
CeAlCuadrado <cealcuadrado@outlook.com>
2021-10-07 12:56:19 -0300

    Agregado cuadro de diálogo de 3 botones

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -16,4 +16,29 @@ export class LayoutComponent implements OnInit {
   openDialogBoxDefault(): void {
     Swal.fire('Título', 'Este es el título del cuadro de diálogo', 'warning');
   }
+
+  openDialogCurrent(): void {
+    Swal.fire({
+      title: "Tres botones",
+      text: "Este es un cuadro de diálogo de tres botones.",
+      icon: "warning",
+      width: "40rem",
+      scrollbarPadding: true,
+      showConfirmButton: true,
+      showDenyButton: true,
+      showCancelButton: true,
+      buttonsStyling: false,
+      customClass: {
+        confirmButton: 'btn btn-success mr-1',
+        denyButton: 'btn btn-primary mr-1',
+        cancelButton: 'btn btn-danger mr-1'
+      }
+    })
+      .then((resolve) => {
+        console.log(resolve);
+      })
+      .catch((reject) => {
+        console.log(reject);
+      });
+  }
 }
